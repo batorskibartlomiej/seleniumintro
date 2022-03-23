e7a462f889c32791eb08c2c3b1d843e017ca9449
batorskibartlomiej <batorskib@gmail.com>
2022-03-23 19:57:16 +0100

    #Selenium grid#2

--- src/test/java/configuration/LocalWebDriverProperties.java
+++ src/test/java/configuration/LocalWebDriverProperties.java
@@ -6,9 +6,7 @@ public class LocalWebDriverProperties {
 
     // Metody zwracają właściwości dla poszczególnych kluczy, analogicznie jak w przypadku AppProperties
 
-    public static BrowserType getLocalBrowser() {
-        return BrowserType.valueOf(ConfigurationProperties.getProperties().getProperty("local.browser"));
-    }
+
 
     public static String getChromeWebDriverLocation() {
         return ConfigurationProperties.getProperties().getProperty("chrome.driver.location");
